c56a1558a4ca14ebe5c7097fde3d4d97d983c2c3
Rasmus Hagberg <rasmus.hagberg@debricked.com>
2021-04-08 17:40:15 +0200

    add maven wrapper

---
+++ java/maven/mavenWrapper.sh
@@ -0,0 +1,24 @@
+if ! [ -d "$1" ] ; then
+	echo "USAGE: "$0" projectRootDirectory"
+	exit 1
+fi
+
+projectRootDirectory="${1%/}"
+
+if ! [ `command -v mvn` > /dev/null 2>&1 ] ; then
+	echo "mvn command not found. Is maven installed?"
+	exit 1
+fi
+
+dependencyDir=`pwd`"/dependencies"
+echo "Copying dependencies to "$dependencyDir" for "$projectRootDirectory
+mvn -B -f $projectRootDirectory dependency:copy-dependencies -DoutputDirectory=$dependencyDir --fail-at-end
+
+echo "Compiling "$projectRootDirectory
+mvn -B -f $projectRootDirectory compile --fail-at-end
+
+pathToSootWrapper="../common/target/SootWrapper-0.1.jar"
+pathToOutputFile="../.debricked-call-graph"
+echo "Running SootWrapper"
+java -jar $pathToSootWrapper -u $projectRootDirectory"/target/classes" -l $dependencyDir -f $pathToOutputFile
+
