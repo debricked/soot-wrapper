9b71a7a6dcd8c803bcd132af6d6babfb26f37377
Rasmus Hagberg <rasmus.hagberg@debricked.com>
2021-04-14 15:28:05 +0200

    add maven dockerfile

--- java/maven/mavenWrapper.sh
+++ java/maven/mavenWrapper.sh
@@ -10,15 +10,15 @@ if ! [ `command -v mvn` > /dev/null 2>&1 ] ; then
 	exit 1
 fi
 
-dependencyDir=`pwd`"/dependencies"
+cwd=`pwd`
+dependencyDir="${cwd%/}/dependencies"
 echo "Copying dependencies to "$dependencyDir" for "$projectRootDirectory
 mvn -B -f $projectRootDirectory dependency:copy-dependencies -DoutputDirectory=$dependencyDir --fail-at-end
 
 echo "Compiling "$projectRootDirectory
 mvn -B -f $projectRootDirectory compile --fail-at-end
 
-pathToSootWrapper="../common/target/SootWrapper-0.1.jar"
-pathToOutputFile="../.debricked-call-graph"
+pathToSootWrapper="vulnfunc/java/common/SootWrapper-0.1.jar"
+pathToOutputFile=".debricked-call-graph"
 echo "Running SootWrapper"
 java -jar $pathToSootWrapper -u $projectRootDirectory"/target/classes" -l $dependencyDir -f $pathToOutputFile
-
