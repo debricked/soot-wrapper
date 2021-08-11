cb9e2dd0cbc0a167f16cae9f09a4212f655cb99a
TeodorBucht1729 <teodor.bucht@gmail.com>
2021-08-11 12:56:36 +0200

    test fix

--- golang/entrypoint.sh
+++ golang/entrypoint.sh
@@ -18,7 +18,7 @@ if ! [ -e "$1/go.mod" ] ; then
 fi
 
 module_dir="${1%/}"
-module_dir=realpath "$module_dir"
+module_dir=dirname "$(realpath $module_dir/go.mod)"
 echo $module_dir
 echo $PWD
 # install dependencies
