2914f7b11a8d97d7f00923c6f2b7bcfc335c7bc4
Rasmus Hagberg <rasmus.hagberg@debricked.com>
2021-09-08 11:05:11 +0200

    use different maven strategy for compiling and moving dependencies

--- java/maven/mavenWrapper.sh
+++ java/maven/mavenWrapper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -xe
 
 if ! [ -d "$1" ] ; then
 	echo "USAGE: "$0" projectRootDirectory"
@@ -15,11 +15,14 @@ exitIfNotInstalled mvn maven
 
 cwd=`pwd`
 dependencyDir="${cwd%/}/dependencies"
-echo "Copying dependencies to "$dependencyDir" for "$projectRootDirectory
-mvn -q -B -f $projectRootDirectory dependency:copy-dependencies -DoutputDirectory=$dependencyDir --fail-at-end
+#echo "Copying dependencies to "$dependencyDir" for "$projectRootDirectory
+#mvn -q -B -f $projectRootDirectory dependency:copy-dependencies -DoutputDirectory=$dependencyDir --fail-at-end
 
-echo "Compiling "$projectRootDirectory
-mvn -q -B -f $projectRootDirectory compile --fail-at-end
+#echo "Compiling "$projectRootDirectory
+#mvn -q -B -f $projectRootDirectory compile --fail-at-end
+
+echo "Compiling and moving dependencies"
+mvn -q -B -f $projectRootDirectory package dependency:copy-dependencies -DoutputDirectory=$dependencyDir -DskipTests
 
 pathToSootWrapper=$pathToCommonDirectory"/SootWrapper-0.1-jar-with-dependencies.jar"
 outputFileName=".debricked-call-graph"
