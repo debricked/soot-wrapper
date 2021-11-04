3cac72fac774c109f84861d6a93101cb89eb99e0
Rasmus Hagberg <rasmus.hagberg@debricked.com>
2021-11-04 14:33:02 +0100

    add support for several userCode and libraryCode args to generic java action

--- java/github-action.sh
+++ java/github-action.sh
@@ -9,7 +9,19 @@ exitIfNotInstalled java
 pathToSootWrapper=$pathToCommonDirectory"/SootWrapper-0.1-jar-with-dependencies.jar"
 outputFileName=".debricked-call-graph"
 
+userCodeArgs=""
+for path in $1
+do
+	userCodeArgs+="-u "$path" "
+done
+
+libraryCodeArgs=""
+for path in $2
+do
+	libraryCodeArgs+="-l "$path" "
+done
+
 echo "Running SootWrapper"
-java -jar $pathToSootWrapper -u $1 -l $2 -f $outputFileName
+java -jar $pathToSootWrapper $userCodeArgs$libraryCodeArgs-f $outputFileName
 
 formatOutput $outputFileName
